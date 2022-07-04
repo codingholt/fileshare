11b12cbb59f7c962a3e246cc54a831281cc0a155
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-07-04 11:46:46 +0200

    init uploads

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
+const multer = require('multer')
+const mongoose = require('mongoose')
 // init express
 const express = require('express')
 const app = express()
+
+const upload = multer({ dest: 'uploads' })
 
 app.set('view engine', 'ejs')
 app.get('/', (req, res) =>{
     res.render('index')
 })
 
+app.post('/upload', upload.single('file'), (req, res) =>{ 
+    res.send('hi👋')
+})
+app.post('upload')
 app.listen(3000)
